7ac6a25ae7d06f1b2827dae65002710f71a48581
Aagman Suri <aagmansuri96@gmail.com>
2020-11-21 23:58:19 +0530

    Update main.c

--- LinkListDisp/main.c
+++ LinkListDisp/main.c
@@ -1,14 +1,21 @@
-//
-//  main.c
-//  LinkListDisp
-//
-//  Created by Aagman Suri on 21/11/20.
-//
-
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+struct Node
+{
+    int data;
+    struct node * next;
+}*first=NULL;        //global variable
+
+// Creating a linked list
+void create(int A[],int n )
+{
+    
+}
+int main()
+{
+    
+    int A[]={3,5,7,10,15};
+    
     return 0;
 }
